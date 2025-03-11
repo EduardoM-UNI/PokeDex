9a711fa4dbb61479f9e2897a02c2acfbeac4aac0
Eduardo Monroy <edu.wap2@gmail.com>
2025-03-11 14:58:11 +0100

    Add mockData

---
+++ PokeDex/PokemonMockData.swift
@@ -0,0 +1,13 @@
+//
+//  PokemonMockData.swift
+//  PokeDex
+//
+//  Created by Eduardo Monroy Husillos on 3/11/25.
+//
+
+import Foundation
+
+struct MockData {
+    
+    static let pokemon = PokemonModel(id: 123, attack: 50, defense: 35, description: "It seductively wiggles its hips as it walks. It can cause people to dance in unison with it.", name: "scyther", imageUrl: "https://firebasestorage.googleapis.com/v0/b/pokedex-bb36f.appspot.com/o/pokemon_images%2FA4390AE6-33EA-4F81-A08B-25F2979158FC?alt=media&token=a225cf16-515e-4b08-a4fe-58bdfaf506b4", type: "flying")
+}
